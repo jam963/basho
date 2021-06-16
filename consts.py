176afc869d937fcab5e9aff7280d6f3c299ab303
Jacob Matthews <jam963@cornell.edu>
2021-06-15 20:46:21 -0400

    created basho

---
+++ consts.py
@@ -0,0 +1,15 @@
+"""
+Constants for bashobot.
+
+"""
+import os
+
+TOKEN = os.getenv("BASHOBOT_TOKEN")
+
+PREFIX = "--"
+
+HEADER = "Writes haikus in the style of Basho"
+
+CORPUS = "basho_corpus.json"
+
+SIZE = 5
