e92b3b0437eed6a51176ef3bbbff9c946e560604
jam963 <jam963@cornell.edu>
2021-07-04 04:08:58 -0400

    Added docstrings to methods and improved style.

--- poem.py
+++ poem.py
@@ -7,9 +7,9 @@ class Poem:
     """
     Each instance represents a Poem from a corpus. Poems have text, which is composed
     of individual words. The text has a lenghth (in words) and a number of lines.
-
-    More attributes may be added to Poem later (rhetorical devices, statistical properties,
-    etc.).
+    One can define an author, title, and/or label, as well as a line delimiter
+    (default is " % "). Poem's constructor expects poem text to have lines
+    delimited by "/"
     """
 
     def __init__(self, text, author=None, title=None, label=None,
@@ -52,6 +52,11 @@ class Poem:
             raise Exception("WordGraph for this Poem does not exist.")
         return self._wg
 
+    def get_graph(self):
+        if self._wg is None:
+            raise Exception("WordGraph for this Poem does not exist.")
+        return self._wg.get_graph()
+
     def get_author(self):
         return self._author
 
@@ -67,6 +72,16 @@ class Poem:
     def get_num_cycles(self):
         return self._num_cycles
 
+    def get_density(self):
+        if self._wg is None:
+            raise Exception("WordGraph for this Poem does not exist.")
+        return self._density
+
+    def get_num_nodes(self):
+        if self._num_nodes is None:
+            raise Exception("WordGraph for this Poem does not exist.")
+        return self._num_nodes
+
     def num_lines(self):
         return self._lines
 
@@ -80,20 +95,19 @@ class Poem:
         self._label = label
 
     def gen_wg(self):
+        """
+        Generate a WordGraph of the Poem.
+        """
         selfset = {self}
         self._wg = WordGraph(selfset)
         self._num_nodes = nx.number_of_nodes(self._wg.get_graph())
         self._density = nx.density(self._wg.get_graph())
 
-    def get_density(self):
-        return self._density
-
-    def get_num_nodes(self):
-        if self._num_nodes is None:
-            raise Exception("WordGraph for this Poem does not exist.")
-        return self._num_nodes
-
     def update_cycles(self):
+        """
+        Updates the number of cycles in the Poem's WordGraph. Raises an
+        Exception if a WordGraph for the Poem does not exist.
+        """
         if self._wg is None:
             raise Exception("WordGraph for this Poem does not exist.")
         cycle_generator = algs.simple_cycles(self._wg.get_graph())
